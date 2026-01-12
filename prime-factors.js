70effb68a42d78ecaee6d45544bd4d67dfdfa755
Thamarai-IND <thamarai.vivid@gmail.com>
2026-01-12 16:18:24 +0530

    prime-factor efficient way implemented

--- prime-factors.js
+++ prime-factors.js
@@ -25,4 +25,46 @@ function isPrime(num) {
     return true
 }
 
-const response1 = PrimeFactor(34);
+// const response1 = PrimeFactor(34);
+
+// Method 2 : Efficient way
+
+function PrimeFactor2(n) {
+    if(n<1) return;
+    for(let i=2; i*i<=n; i++) {
+        while(n % i === 0) {
+            console.log(i);
+            n = n / i;
+        }
+    }
+    if (n > 1) console.log(n);
+} // Time Complexity - O(n^1/2 log n)
+
+PrimeFactor2(450)
+
+// Method 3 :  More Efficient Way
+
+function PrimeFactor3(n) {
+    if(n<1) return;
+    while(n%2 === 0) {
+        console.log(2);
+        n=n/2
+    }
+    while(n%3 === 0) {
+        console.log(3);
+        n=n/3
+    }
+    for(let i=5; i*i<=n; i=i+6) {
+        while(n % i === 0) {
+            console.log(i);
+            n = n / i;
+        }
+        while(n % (i+2) === 0) {
+            console.log(i+2);
+            n = n / (i+2);
+        }
+    }
+    if (n > 3) console.log(n);
+} // Time Complexity - O(n^1/2 log n)
+
+PrimeFactor3(450)
